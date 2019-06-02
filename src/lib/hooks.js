1f876e531bc8471da8e4c31d1ef2b7d13e72c028
Minh <hlminh2000@gmail.com>
2019-06-02 09:25:24 -0400

    allows playback of effects

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 export const createStore = () => {
     const states = [];
     const stateHooks = {};
     const actions = [];
     const playbackState = {
         playbackTimeout: undefined,
+        isPlayingEffect: false,
     }
     const stopPlayback = () => {
         clearTimeout(playbackState.playbackTimeout)
@@ -37,9 +39,6 @@ export const createStore = () => {
 
 const StoreContext = React.createContext(createStore())
 
-export const StoreProvider = StoreContext.Provider
-export const useStore = () => React.useContext(StoreContext)
-
 export const useStateWithId = (id, initial) => {
     const store = useStore()
     const {
@@ -68,29 +67,52 @@ export const useStateWithId = (id, initial) => {
         return () => delete stateHooks[id]
     }, [])
     const setState = (newState) => {
-        reactSetState(newState)
-        actions.push({
-            id,
-            newState,
-            timeStamp: Date.now()
-        })
-        states.push(
-            Object.entries(stateHooks).map(
-                ([key, {state}]) => ({
-                    id:key, 
-                    state: key === id ? newState : state
-                })
+        if(!store.playbackState.isPlayingEffect) {
+            actions.push({
+                id,
+                newState,
+                timeStamp: Date.now()
+            })
+            states.push(
+                Object.entries(stateHooks).map(
+                    ([key, {state}]) => ({
+                        id:key, 
+                        state: key === id ? newState : state
+                    })
+                )
             )
-        )
+            return reactSetState(newState)
+        }
     }
     return [state, setState]
+}
+
+export const useReducerWithId = (id, reducer, initial, init) => {
+    const [state, reactDispatch] = React.useReducer(reducer, initial)
+    const store = useStore()
+    const dispatch = (reducerAction) => {
+        const newState = reducer(state, reducerAction)
+        store.actions.push({
+            id,
+            newState,
+            timeStamp: Date.now(),
+            reducerAction
+        })
+        return reactDispatch(reducerAction)
+    }
+    return [state, dispatch]
 }
 
 export const useEffect = (effect, watch) => {
     const store = useStore()
     React.useEffect(() => {
-        if(!store.playbackState.playbackTimeout) {
-            effect()
+        effect()
+        if(store.playbackState.playbackTimeout) {
+            store.playbackState.isPlayingEffect = true
         }
+        return () => store.playbackState.isPlayingEffect = false
     }, watch)
 }
+
+export const StoreProvider = StoreContext.Provider
+export const useStore = () => React.useContext(StoreContext)
